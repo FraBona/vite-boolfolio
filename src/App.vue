bbe13e65bc13fa35c886583331909a9b26fd7479
FraBona <francescobonafede08@gmail.com>
2024-01-15 16:09:30 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script >
+  export default {
+    data(){
+      return{
+        projects: [],
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    Hello
+  </div>
+</template>
+
+<style lang="scss">
+@use './style/general.scss'
+</style>
