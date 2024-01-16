52362b6305f20faca30a30aedf21a4a77e25ccd1
FraBona <francescobonafede08@gmail.com>
2024-01-16 15:08:06 +0100

    ADD route Contact

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,6 +9,9 @@
           <li>
             <router-link :to="{name: 'projects.index'}">Portfolio</router-link>
           </li>
+          <li>
+            <router-link :to="{name: 'contacts'}">Contatti</router-link>
+          </li>
         </ul>
       </nav>
     </div>
---
+++ src/pages/Contact.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h1>Pagina contatti</h1>
+    <div class="container">
+      <form action="" method="POST">
+        <input type="text" placeholder="name" name="name">
+        <input type="email" placeholder="email" name="email">
+        <textarea name="maessage" id="" cols="30" rows="10" placeholder="messaggio"></textarea>
+        <input type="submit" value="Invia">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
