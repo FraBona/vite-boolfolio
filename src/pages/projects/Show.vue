36e8b41cb0beecd541357680008328523805264c
FraBona <francescobonafede08@gmail.com>
2024-01-16 15:58:20 +0100

    ADD call axios  and print title and content of project

--- src/pages/projects/Show.vue
+++ src/pages/projects/Show.vue
@@ -1,25 +1,36 @@
 <template>
-  <div class="container">
-    <h1>{{  }}</h1>
-    <p>{{slug}}</p>
+  <div v-if="project"> 
+    <div class="container">
+    <h1>{{project.title}}</h1>
+    <p>{{project.slug}}</p>
   </div>
   <div class="container">
-    <p>contenuto del post</p>
+    <p>{{project.content}}</p>
+  </div>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
   export default{
     data(){
       return{
-
+        project: null,
       }
     },
     props: {
       slug: String,
     },
+    methods: {
+      fetchProject(){
+        axios.get(`http://127.0.0.1:8000/api/projects/${this.slug}`)
+        .then(res =>{
+          this.project = res.data.project
+        })
+      }
+    },
     created(){
-      const slug = this.$route.params.slug
+      this.fetchProject();
     }
   }
 </script>
